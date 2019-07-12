0ef1dd7f873c51fd6a4af386111b9b316e034091
alelulu <ale.silvasuazo@gmail.com>
2019-07-12 08:53:26 -0400

    first changes

---
+++ burger-queen/src/components/prices.js
@@ -0,0 +1,17 @@
+import React, { Component} from 'react';
+
+//Creamos componente para lista de precios
+class prices extends Component {
+  //Método render para mostrar en pantalla, retorna la interfaz (html)
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>hola</li>
+          <li>hole</li>
+          <li>holi</li>
+        </ul>
+      </div>
+    )
+  }
+}
