66cb53621bb3df9ea31885957e53f714fc11d215
alelulu <ale.silvasuazo@gmail.com>
2019-07-13 20:02:08 -0400

    list component created

---
+++ burger-queen/src/components/list.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Item from './item'
+import { menu } from '../data/menu'
+import './list.css';
+
+class List extends Component {
+  render() {
+    return (
+      <div className="list-container">
+        <div className="row name-container">
+          <div className="col s1 client-container">
+            <i className="fas fa-user-alt client-icon"></i>
+          </div>
+          <div className="col s11 input-field">
+            <input className="input-name" type="text" placeholder="Nombre del cliente"/>
+          </div>
+          
+        </div>
+
+        <div className="row">
+          <div className="items-list">
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+            <Item name={menu.breakfast[1].item}/>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col s2 push-s7">
+            <h6>Total: </h6>
+          </div>
+          <div className="col s3 push-s7">
+            <h5 className="total-price">$ 100</h5>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default List
