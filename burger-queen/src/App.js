854e0ef69c14aea02a73a10d4ad44cd232a148f9
natiacostap <nati_aap@hotmail.com>
2019-07-17 14:17:59 -0400

    se muestran los botones de con items de desayuno sacados desde menu.json

--- burger-queen/src/App.js
+++ burger-queen/src/App.js
@@ -3,14 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import List from './components/list'
 import './components/list.css'
+import OptionsMenu from "./components/option-menu"
+import menu from './data/menu.json'
 
 function App() {
   return (
     <div className="App">
+         <OptionsMenu title="hola mundo"/>
       <h1>hola</h1>
-      <List />
+      <List /> 
     </div>
   );
 }
-
 export default App;
