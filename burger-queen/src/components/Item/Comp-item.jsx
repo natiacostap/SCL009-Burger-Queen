5a69f8a4f1a9d2bcec249ff839ee7e4cd04e952a
natiacostap <nati_aap@hotmail.com>
2019-07-22 20:36:25 -0400

    aceptando cambios desde el merge(iconos en json, elimine mesere.js para usar mesere.jsx

--- burger-queen/src/components/Item/Comp-item.jsx
+++ burger-queen/src/components/Item/Comp-item.jsx
@@ -14,12 +14,7 @@ class Item extends Component {
         <tr className="row">
           <td className="col-8 item-text">{this.props.name}</td>
           <td className="col-1 count-text">{this.props.quantity}</td>
-<<<<<<< HEAD
-          <td className="col-1 price-text">{this.props.price}</td>
-          <td className="col-3"><button className="delete-btn" type="button"><i className="fas fa-times"></i></button></td>
-=======
           <td className="col-3"><button className="delete-btn" onClick={()=>this.props.handleRemove(this.props.item,this.props.quantity,this.props.price)}type="button"><i className="fas fa-times"></i></button></td>
->>>>>>> 70a8346de2291bf8eb7a6b9aab4ef3995c91bb4f
         </tr>
       </div>
     )
