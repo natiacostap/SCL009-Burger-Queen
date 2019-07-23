fca007292ef9bd0474b1131241302e42cae8c6dc
natiacostap <nati_aap@hotmail.com>
2019-07-23 18:24:56 -0400

    quitamos precio de lista y de item

--- burger-queen/src/components/Item/Comp-item.jsx
+++ burger-queen/src/components/Item/Comp-item.jsx
@@ -10,10 +10,8 @@ class Item extends Component {
       <div className="item-container">
         <tr className="row">
           <td className="col-8 item-text">{this.props.name}</td>
-          <td className="col-1 price-text">{this.props.price}</td>
           <td className="col-1 count-text">{this.props.quantity}</td>
           <td className="col-3"><button className="delete-btn" onClick={() => this.props.handleRemove(this.props.id)}type="button"><i className="fas fa-times"></i></button></td>
-
         </tr>
       </div>
     )
