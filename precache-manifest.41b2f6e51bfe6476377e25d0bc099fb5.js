763224861e6d2095f0afa4b188e2e4e50c7b0053
natiacostap <nati_aap@hotmail.com>
2019-07-26 14:01:26 -0400

    Updates

---
+++ precache-manifest.41b2f6e51bfe6476377e25d0bc099fb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb01c82b60c3cb0cd930b4a4f4ee44a",
+    "url": "/SCL009-Burguer-Queen/index.html"
+  },
+  {
+    "revision": "4c118d8f09e2ffe78e53",
+    "url": "/SCL009-Burguer-Queen/static/css/main.75600a37.chunk.css"
+  },
+  {
+    "revision": "bf08420060aea967d678",
+    "url": "/SCL009-Burguer-Queen/static/js/2.ecc03e40.chunk.js"
+  },
+  {
+    "revision": "4c118d8f09e2ffe78e53",
+    "url": "/SCL009-Burguer-Queen/static/js/main.0ea643e9.chunk.js"
+  },
+  {
+    "revision": "bfa923c0039a074aaf3b",
+    "url": "/SCL009-Burguer-Queen/static/js/runtime~main.7a13f7bd.js"
+  }
+]);
