be70b31143a68192c3a2e36746904141a5e0aa1a
alelulu <ale.silvasuazo@gmail.com>
2019-07-26 10:11:17 -0400

    actualización de readme y cocina

--- burger-queen/src/views/Cocina.js
+++ burger-queen/src/views/Cocina.js
@@ -2,17 +2,18 @@ import React, {Component} from 'react';
 import Navbar from '../components/Navbar/Comp-navbar'
 
 class Cocina extends Component{
-    render(){
-        return(
-            <div className="showCocina">
-                <div className="header">
-					<Navbar/>
+	render(){
+		return(
+			<div className="showCocina">
+				<div className="header">
+				<Navbar/>
 				</div>
-                <h1>En contrucción</h1>
 
-            </div>
-        )
-    }
+				<h1>Aquí no hay nada ¡El gato se comió todo!</h1>
+        <img className="fat-cat" src="https://i.ibb.co/xDQy03W/culpable.png" alt="fat-cat"/>
+			</div>
+		)
+	}
 
 }
 export default Cocina
